88cda65d6d3f559d9ba1bb5e3d0a20cf1718cfbe
holtjma <jholt@hudsonalpha.org>
2019-02-09 17:29:04 -0600

    added basic example

---
+++ example/README.md
@@ -0,0 +1,20 @@
+# Examples
+
+## Run _E. coli_ Example
+### Commands
+1. Go to a directory to store fmlrc and clone this repository:
+```bash
+git clone https://github.com/holtjma/fmlrc.git
+```
+2. Change into the fmlrc examples directory:
+```bash
+cd fmlrc/examples
+```
+3. Run the examples script.  This will automatically install ropebwt2 to a local path, download short- and long-read _E. coli_ data, build the multi-string BWT using ropebwt2 and fmlrc-convert, and lastly run use fmlrc to correct the first 400 reads in the long-read data:
+```bash
+./run_example.sh
+```
+
+### Outputs
+1. `./ecoli_comp_msbwt.npy` - this contains the run-length encoded multi-string BWT using the same encoding as the [msbwt](https://github.com/holtjma/msbwt) python package.  Note: if you wish to use this with msbwt, it will need to be placed in it's own directory and renamed to `comp_msbwt.npy` in that directory.  For more information, please refer to the msbwt wiki pages.
+2. `./corrected_final.fa` - this contains the first 400 long reads after correction using fmlrc.
---
+++ example/run_example.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#Download this script via "git clone https://github.com/holtjma/fmlrc.git"
+#cd to "fmlrc/examples" and run "./run_example.sh"
+
+#download and build ropebwt2
+if [ ! -f ./ropebwt2/ropebwt2 ]; then
+    git clone https://github.com/lh3/ropebwt2.git
+    cd ropebwt2; make; cd ..
+fi
+
+#download and build fmlrc
+if [ ! -f ../fmlrc ]; then
+    cd ..; make; cd example
+fi
+
+#download short-read ecoli
+if [ ! -f s_6_1.fastq.gz ]; then
+    curl -o s_6_1.fastq.gz http://spades.bioinf.spbau.ru/spades_test_datasets/ecoli_mc/s_6_1.fastq.gz
+fi
+if [ ! -f s_6_2.fastq.gz ]; then
+    curl -o s_6_2.fastq.gz http://spades.bioinf.spbau.ru/spades_test_datasets/ecoli_mc/s_6_2.fastq.gz
+fi
+
+#download long-read ecoli and convert to fasta format
+if [ ! -f PacBioCLR/PacBio_10kb_CLR.fasta ]; then
+    curl -o Ecoli_MG1655_pacBioToCA.tgz http://files.pacb.com/datasets/secondary-analysis/e-coli-k12-de-novo/1.3.0/Ecoli_MG1655_pacBioToCA.tgz
+    tar -xvzf Ecoli_MG1655_pacBioToCA.tgz
+    awk 'NR%4==1||NR%4==2' ./PacBioCLR/PacBio_10kb_CLR.fastq | tr "@" ">" > ./PacBioCLR/PacBio_10kb_CLR.fasta
+fi
+
+#build the bwt
+if [ ! -f ./ecoli_comp_msbwt.npy ]; then
+    mkdir temp
+    gunzip -c s_6_?.fastq.gz | awk "NR % 4 == 2" | sort -T ./temp | tr NT TN | ./ropebwt2/ropebwt2 -LR | tr NT TN | ../fmlrc-convert ./ecoli_comp_msbwt.npy
+fi
+
+#run fmlrc
+NUM_PROCS=4
+../fmlrc -p $NUM_PROCS -V -e 400 ./ecoli_mc_msbwt/comp_msbwt.npy ./PacBioCLR/PacBio_10kb_CLR.fasta ./corrected_final.fa
